9e46dae8f77e5febf99c814df7eba8175178834a
anthonykhong <anthonykhong95200@gmail.com>
2023-05-19 01:36:27 +0200

    create postcss file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./pages/App";
-import "./styles/tailwind.css";
+import "./style/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import viteLogo from '../assets/trelandar.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-
+import { useState } from "react";
+import viteLogo from "../assets/trelandar.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -13,12 +12,9 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-
       </div>
       <h1 className="text-3xl font-bold underline">Trelandar</h1>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <h1 class="text-3xl text-blue-500 font-bold underline">Hello world!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,7 +27,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
