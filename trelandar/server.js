469342733e98ad0f3460df6af9d928a13bce2b59
yutaokkots <yuta.okkotsu@gmail.com>
2023-05-17 19:15:51 -0600

    scaffolded app

---
+++ trelandar/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+// require('dotenv').config();
+// require('./config/database')
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', function(req, res) {
+res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+console.log(`Express app running on port ${port}`)
+});
