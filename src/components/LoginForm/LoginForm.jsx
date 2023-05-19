2b42d41de2ca2c90ec6dbd29dd6456ecdd154acb
Sally-Kam <skam2000@gmail.com>
2023-05-18 22:10:58 -0400

    update login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import React from 'react'
-import "./LoginForm.css"
+
 export default function LoginForm() {
     const [credentials, setCredentials] = useState({
         email: '',
