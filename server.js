b0a2c00772f473aca53b901b3891d3b6f7e338d2
xkrzypandax <xkrzypandax@gmail.com>
2023-05-18 18:13:14 -0700

    New Packages

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.use(require("./config/checkTokens"));
+
+// Error handler to check if route exists
+app.use(function (req, res) {
+    console.log(`route: ${req.path} does not exist`);
+    res.status(404, "route does not exist");
+  });
+
 app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
