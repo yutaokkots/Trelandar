cc2166f66267b8cb49c2d11ed45ea9bf5cd1745c
EricHungGA <ehung303@gmail.com>
2023-05-18 17:15:32 -0700

    fixing merge with prisma

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
+    
 
 const port = process.env.PORT || 3000;
 
