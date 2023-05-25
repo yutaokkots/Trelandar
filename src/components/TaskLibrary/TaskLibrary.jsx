40e90c6d31eba30a780101dac52f14b88aafbc33
yutaokkots <yuta.okkotsu@gmail.com>
2023-05-25 17:06:28 -0600

    integrated react-front-end layout updates

--- src/components/TaskLibrary/TaskLibrary.jsx
+++ src/components/TaskLibrary/TaskLibrary.jsx
@@ -31,19 +31,19 @@ const tasks = [
 export default function TaskLibrary() {
   return (
     <>
-    <div className='flex flex-col'>
-      <div className='text-center pt-[1vh]'>TaskLibrary</div>
-      {
-        tasks.map((task, idx) => {
-          return <div className='flex flex-row justify-between items-center w-[12vw] mx-[1vw] my-[1vh] px-[1vmin] py-[1vmin] border-black border-[.1vmin] rounded-[5px]'>
-                    <TaskItem task={task} key={idx}/>
-                    <div style={{backgroundColor: task.color}} className='w-[2vmin] h-[2vmin] bg-[slate-500] rounded-[50%]'>
+      <div className='flex flex-col'>
+        <div className='text-center pt-[1vh]'>TaskLibrary</div>
+        {
+          tasks.map((task, idx) => {
+            return <div key={idx} className='flex flex-row justify-between items-center w-[12vw] mx-[1vw] my-[1vh] px-[1vmin] py-[1vmin] border-black border-[.1vmin] rounded-[5px]'>
+                      <TaskItem task={task}/>
+                      <div style={{backgroundColor: task.color}} className='w-[2vmin] h-[2vmin] bg-[slate-500] rounded-[50%]'>
 
-                    </div>
+                      </div>
                   </div>
-        })
-      }
-    </div>
+          })
+        }
+      </div>
     </>
   )
 }
