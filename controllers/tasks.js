db2b5172479713ec7856cff53ed7c82a146b24d2
yutaokkots <yuta.okkotsu@gmail.com>
2023-05-28 19:28:20 -0600

    created several functions for routes related to the task/taskcategories. these functions are (located in the controllers) are not tested

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,22 +1,89 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+// getAll() -> retrieves all task categories for a given user
+// find the userid of the user that is logged in
+// get the tasks
+async function getAll(req, res){
+    try {
+        const allTasks = await prisma.TaskCategory.getAll({
+            where: {
+                user: req.user.id // <- this code is not correct
+            }
+        })
+        res.json(allTasks)
+    } catch(error) {
+        console.log(error)
+        res.status(400).json('Could not process')
+    }
+}
 
-async function initializeTaskCategory(user) {
+
+// createTaskCategory() -> creates a task-category for a user by:
+// (1) finding the user id of the user that is logged in
+// (2) creating a taskCategory based on the information provided
+// what will the req.body look like? 
+async function createTaskCategory(req, res) {
     const userId = await prisma.user.findUnique({
         where: {
-            user: user
+            user: req.body.user
         }
     })
     const taskCategory = await prisma.TaskCategory.create({
         data: {
+            name: req.body.name,
+            detail: req.body.detail,
+            color: req.body.color,
             user: userId,
-            userId: user.id,
         }    
     })
+}
 
+// Drag and drop feature, on front end:
+// (1) drag
+// (2) drop
+// (3) execute function to prompt user to enter information
+// (4) after submitting, request sent to backend
+
+
+// createTask() -> creates a new task based on the id of the
+// task category. 
+async function createTask(req, res){
+    try{
+        const newTask = await prisma.task.create({
+            data: {
+                notes: req.body.notes,
+                tasktype: req.body.tasktype,
+                startTime: req.body.startTime,
+                endTime: req.body.endTime
+            }
+        }) 
+        // return 
+        res.status(201).json('Added new task')
+    }catch(error){
+        console.log(error)
+        res.status(400).json('Could not process')
+    }
+}
+
+
+// editTask() -> creates a new task based on the id of the
+// task category. 
+async function editTask(req, res){
+    try{
+        await prisma.task.findUnique({
+            where: {
+                id: req.params.id
+            }
+        })
+    }catch(error){
+        console.log(error)
+    }
 }
 
 module.exports = {
-    initializeTaskCategory
+    createTaskCategory,
+    getAll,
+    createTask,
+    editTask
 }
---
+++ routes/tasks.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const tasksCtrl = require('../controllers/tasks.js');
+
+////////// Task Categories
+// GET
+// api/tasks/getAll
+
+// -> get all task categories
+// -> this function will retreive all of the user's task categories
+// ane make sure it is refreshed via a state change
+router.get('getall', tasksCtrl.getAll)
+
+
+// POST
+// api/tasks/create
+// -> creation of new task category
+// when a user adds a new task category (left-hand side of the dashboard)
+// after pressing a button, the user is prompted to enter information for
+// a new task category. 
+router.post('/create', tasksCtrl.createTaskCategory)
+
+
+////////// Tasks 
+// POST
+// api/tasks/:id/task/create
+// -> creation of new task
+// a user can drag a task category from the left-hand side
+// to anywhere on the calendar, which prompts the user to create
+// a new task. 
+
+router.post('/:id/task/create', tasksCtrl.createTask)
+
+
+// PUT
+// api/tasks/:id/task/edit
+// -> editing a task
+// a user can edit the task, for exampole, the notes, task-type, 
+// the start time, and end time, as well as whether the
+// task is completed or not
+router.put('/:id/task/edit', tasksCtrl.editTask)
