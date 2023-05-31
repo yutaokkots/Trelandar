b2f4c4fb47cd173efe9f6e69bd2389e482327b60
Sally-Kam <skam2000@gmail.com>
2023-05-30 20:35:59 -0400

    add meet the team carousel

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutPage() {
   return (
     <main>
-      <div className="bg-[#FCDEBE] h-screen">
+      <div className="bg-[#FCDEBE]">
         <div className="flex flex-col">
           <h1 className="flex justify-center font-bold text-5xl text-[#5e5768] py-16">
             About Us
@@ -23,8 +23,246 @@ export default function AboutPage() {
         <div>
           <h1 className="flex justify-center font-bold text-3xl text-[#5e5768] py-14">
             Meet the Team
-          </h1>  
+          </h1> 
+          <div className="flex  items-center justify-center pb-10">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-96 w-72">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1682897191/341935091_933990431174626_1981406993847747898_n_g6ug4s.jpg"
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="text-[#5e5768] text-3xl font-bold">
+                Yuta Okkotsu
+              </h1>
+              <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Lorem yesssipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              </p>
+              <div className="flex">
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/yutaokkots"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/yutaokkotsu/"
+                  >
+                    LinkedIn
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-96 w-72">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1685493227/eric_d4sboa.jpg"
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="text-[#5e5768] text-3xl font-bold">
+                Eric Hung
+              </h1>
+              <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Lorem yesssipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              </p>
+              <div className="flex justify-even">
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/EricHungGA"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/erichungdev/"
+                  >
+                    LinkedIn
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-96 w-72">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1685493321/me_n0lj84.jpg"
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="text-[#5e5768] text-3xl font-bold">
+                Anthony Khong
+              </h1>
+              <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Lorem yesssipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              </p>
+              <div className="flex justify-even">
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/anthonykhong"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/anthonykhong956/"
+                  >
+                    LinkedIn
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-96 w-72">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1682899000/May-01-2023_01-56-28_e19no1.gif"
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="text-[#5e5768] text-3xl font-bold">
+                Vilem Mckael
+              </h1>
+              <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Lorem yesssipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              </p>
+              <div className="flex justify-even">
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Vilem-McKael"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/vilem-mckael/"
+                  >
+                    LinkedIn
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-96 w-72">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1684192740/login_page_fh0fcw.gif"
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="text-[#5e5768] text-3xl font-bold">
+                Sally Kam
+              </h1>
+              <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Lorem yesssipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              </p>
+              <div className="flex">
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/sally-kam"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/sallykam/"
+                  >
+                    LinkedIn
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-96 w-72">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1684192740/login_page_fh0fcw.gif"
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="text-[#5e5768] text-3xl font-bold">
+                Woonchan Jung
+              </h1>
+              <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Lorem yesssipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              </p>
+              <div className="flex">
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/woonchanjung"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:text-purple-500 shadow shadow-black/60">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/woonchanjung/"
+                  >
+                    LinkedIn
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+        </div>        
       </div>
     </main>
   );
