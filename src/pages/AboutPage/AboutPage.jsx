81e367a171f154b4d67f42136e230b5114dfbb57
Sally-Kam <skam2000@gmail.com>
2023-05-31 16:10:46 -0400

    remove carousel in aboutpage

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -44,18 +44,11 @@ export default function AboutPage() {
           <h1 className="flex justify-center font-bold text-3xl text-[#5e5768] py-14">
             Meet the Team
           </h1>
-          <div
-            id="default-carousel"
-            className="relative w-full justify-center h-96 w-72 pb-10 px-20"
-            data-carousel="slide"
-          >
-            <div className="relative text-center h-56 overflow-hidden rounded-lg md:h-96">
+
+            
               <div className="flex  items-center justify-center pb-10">
                 <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
-                  <div
-                    className="hidden duration-700 ease-in-out"
-                    data-carousel-item
-                  >
+           
                     <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
                       <div className="h-96 w-72">
                         <img
@@ -96,11 +89,8 @@ export default function AboutPage() {
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div
-                    className="hidden duration-700 ease-in-out"
-                    data-carousel-item
-                  >
+                 
+               
                     <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
                       <div className="h-96 w-72">
                         <img
@@ -141,11 +131,8 @@ export default function AboutPage() {
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div
-                    className="hidden duration-700 ease-in-out"
-                    data-carousel-item
-                  >
+                  
+               
                     <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
                       <div className="h-96 w-72">
                         <img
@@ -186,11 +173,8 @@ export default function AboutPage() {
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div
-                    className="hidden duration-700 ease-in-out"
-                    data-carousel-item
-                  >
+                
+            
                     <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
                       <div className="h-96 w-72">
                         <img
@@ -231,12 +215,9 @@ export default function AboutPage() {
                         </div>
                       </div>
                     </div>
-                  </div>
+                  
 
-                  <div
-                    className="hidden duration-700 ease-in-out"
-                    data-carousel-item
-                  >
+          
                     <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
                       <div className="h-96 w-72">
                         <img
@@ -277,11 +258,8 @@ export default function AboutPage() {
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div
-                    className="hidden duration-700 ease-in-out"
-                    data-carousel-item
-                  >
+             
+          
                     <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
                       <div className="h-96 w-72">
                         <img
@@ -322,96 +300,14 @@ export default function AboutPage() {
                         </div>
                       </div>
                     </div>
-                  </div>
+                
                 </div>
               </div>
-            </div>
-            <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="true"
-                aria-label="Slide 1"
-                data-carousel-slide-to="0"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 2"
-                data-carousel-slide-to="1"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 3"
-                data-carousel-slide-to="2"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 4"
-                data-carousel-slide-to="3"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 5"
-                data-carousel-slide-to="4"
-              ></button>
-            </div>
-            <button
-              type="button"
-              className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-              data-carousel-prev
-            >
-              <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 19l-7-7 7-7"
-                  ></path>
-                </svg>
-                <span className="sr-only">Previous</span>
-              </span>
-            </button>
-            <button
-              type="button"
-              className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-              data-carousel-next
-            >
-              <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M9 5l7 7-7 7"
-                  ></path>
-                </svg>
-                <span className="sr-only">Next</span>
-              </span>
-            </button>
-          </div>
+            
+
+
+
+          
         </div>
       </div>
       <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
