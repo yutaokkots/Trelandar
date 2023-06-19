447f06cd925716a53e309c2a0798a64b5abc8085
anthonykhong <anthonykhong95200@gmail.com>
2023-06-19 14:49:08 -0700

    implement loading animation

--- src/components/WeeklySchedule/WeeklySchedule.jsx
+++ src/components/WeeklySchedule/WeeklySchedule.jsx
@@ -1,53 +1,52 @@
-import React from 'react'
-import TaskItem from '../TaskItem/TaskItem'
-import Weekday from '../Weekday/Weekday'
+import React from "react";
+import TaskItem from "../TaskItem/TaskItem";
+import Weekday from "../Weekday/Weekday";
 import { useState, useEffect } from "react";
-import * as tasksAPI from '../../utilities/tasks-api'
+import * as tasksAPI from "../../utilities/tasks-api";
 
 // from datetime import date
 
-export default function WeeklySchedule({categories}) {
-
+export default function WeeklySchedule({ categories }) {
   const weekdayNames = [
-    'Monday',
-    'Tuesday',
-    'Wendesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-  ]
+    "Monday",
+    "Tuesday",
+    "Wendesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
   const [days, setDays] = useState([
     {
-      day: 'Monday',
+      day: "Monday",
       tasks: [],
     },
     {
-      day: 'Tuesday',
+      day: "Tuesday",
       tasks: [],
     },
     {
-      day: 'Wednesday',
+      day: "Wednesday",
       tasks: [],
     },
     {
-      day: 'Thursday',
+      day: "Thursday",
       tasks: [],
     },
     {
-      day: 'Friday',
+      day: "Friday",
       tasks: [],
     },
     {
-      day: 'Saturday',
+      day: "Saturday",
       tasks: [],
     },
     {
-      day: 'Sunday',
+      day: "Sunday",
       tasks: [],
     },
-  ])
+  ]);
 
   const [fetchedTasks, setFetchedTasks] = useState(false);
 
@@ -58,23 +57,23 @@ export default function WeeklySchedule({categories}) {
       try {
         const allTasks = await tasksAPI.getTasks();
         setTasks(allTasks);
-        const updatedDays = [...days]
+        const updatedDays = [...days];
         for (let i = 0; i < allTasks.length; i++) {
           const thisDate = new Date(allTasks[i].startTime);
           const dayOfTheWeek = thisDate.getDay();
           // console.log('task: ', tasks[i], 'this date: ', thisDate, 'day of the week: ', dayOfTheWeek)
           const dayIndex = (dayOfTheWeek + 6) % 7; // 0 --> 6 % 7 == 6, for sunday //
           const dayObj = updatedDays[dayIndex];
-          if (!dayObj.tasks.find((task) => task.id === allTasks[i].id)) { // this is DUMMY THICC memory intensive so let's find another solution eventually
+          if (!dayObj.tasks.find((task) => task.id === allTasks[i].id)) {
+            // this is DUMMY THICC memory intensive so let's find another solution eventually
             dayObj.tasks = [...dayObj.tasks, allTasks[i]];
           }
         }
         setDays(updatedDays);
         setFetchedTasks(true);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-      
     }
     getAllTasks(); //hahasdfasfsf
   }, []);
@@ -85,16 +84,27 @@ export default function WeeklySchedule({categories}) {
 
   return (
     <>
-    <div className=' flex flex-row justify-evenly'>
-      {!fetchedTasks ? 
-      <div>Calendar loading. . . </div>
-      :
-      <>
-      {days.map((day, idx) => <Weekday tasks={day.tasks} categories={categories} day={day.day} key={idx}/>)
-      }
-      </>
-      }
-    </div>
+      <div className=" flex flex-row justify-evenly">
+        {!fetchedTasks ? (
+          <div className="mt-80">
+            <img
+              className="h-60"
+              src="https://res.cloudinary.com/diw7vmgum/image/upload/v1687211240/loading-animation_jckhrz.gif"
+            />{" "}
+          </div>
+        ) : (
+          <>
+            {days.map((day, idx) => (
+              <Weekday
+                tasks={day.tasks}
+                categories={categories}
+                day={day.day}
+                key={idx}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
