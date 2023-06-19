ef3b0a2e138851c5e418f7c3345d28cda124809e
anthonykhong <anthonykhong95200@gmail.com>
2023-06-19 15:16:04 -0700

    edit tasklibrary page

--- src/components/TaskLibrary/TaskLibraryOriginal.jsx
+++ src/components/TaskLibrary/TaskLibraryOriginal.jsx
@@ -29,19 +29,20 @@ const tasks = [
   },
 ];
 
-const dummy_taskCategories = [
-  
-]
+const dummy_taskCategories = [];
 
-export default function TaskLibrary({categories}) {
+export default function TaskLibrary({ categories }) {
   return (
     <>
       <div className="flex flex-col items-center h-[90vh]">
         <div className="flex justify-between items-center px-6 py-3">
-        <h2 className="flex items-center text-center">Task Library</h2>
-        <button>
-        <img className="h-6" src="https://res.cloudinary.com/diw7vmgum/image/upload/v1685131977/plus_fftfrp.png" />
-        </button>
+          <h2 className="flex items-center text-center">Task Library</h2>
+          <button>
+            <img
+              className="h-6"
+              src="https://res.cloudinary.com/diw7vmgum/image/upload/v1685131977/plus_fftfrp.png"
+            />
+          </button>
         </div>
         {categories.map((category, idx) => {
           return (
@@ -59,22 +60,21 @@ export default function TaskLibrary({categories}) {
         })}
 
         <div>
-            {categories.map((category, idx) => {
-              return (
+          {categories.map((category, idx) => {
+            return (
+              <div
+                key={idx}
+                className="hover:bg-[#5E5768] hover:text-white flex flex-row justify-between items-center  w-[12vw] mx-[1vw] my-[1vh] px-[1vmin] py-[1vmin] border-black border-[.2vmin] rounded-[5px]"
+              >
+                <CategoryItemDnD category={category} />
                 <div
-                  key={idx}
-                  className="hover:bg-[#5E5768] hover:text-white flex flex-row justify-between items-center  w-[12vw] mx-[1vw] my-[1vh] px-[1vmin] py-[1vmin] border-black border-[.2vmin] rounded-[5px]"
-                >
-                  <CategoryItemDnD category={category} />
-                  <div
-                    style={{ backgroundColor: category.color }}
-                    className="w-[2vmin] h-[2vmin] bg-[slate-500] rounded-[50%]"
-                  ></div>
-                </div>
-              );
-            })}
+                  style={{ backgroundColor: category.color }}
+                  className="w-[2vmin] h-[2vmin] bg-[slate-500] rounded-[50%]"
+                ></div>
+              </div>
+            );
+          })}
         </div>
-
       </div>
     </>
   );
