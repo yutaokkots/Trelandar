9ace8ca589ea6eb571dd4d69aef1912b6aa74d76
anthonykhong <anthonykhong95200@gmail.com>
2023-06-19 15:17:53 -0700

    add NewTaskPopup file

---
+++ src/components/Popups/NewTaskPopup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import from "./Modal.css";
+
+export default function NewTaskPopup() {
+  return (
+    <main>
+      <div id="popup" className="bg-blue-100 w-6/12 rounded-md">
+        <div className="">Vilem</div>
+      </div>
+    </main>
+  );
+}
