da6933389e3d527006760f58397f5f0b84f0a457
anthonykhong <anthonykhong95200@gmail.com>
2023-06-19 15:19:05 -0700

    add popup in tasklibrary

--- src/components/TaskLibrary/TaskLibrary.jsx
+++ src/components/TaskLibrary/TaskLibrary.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import CategoryItem from "../CategoryItem/CategoryItem";
 import CategoryItemDnD from "../CategoryItem/CategoryItemDnD";
-import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
-
-export default function TaskLibrary({categories}) {
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import Popup from "reactjs-popup";
+import NewTaskPopup from "../Popups/NewTaskPopup";
+import "../Popups/Modal.css";
+export default function TaskLibrary({ categories }) {
   return (
     <>
-
       <div className="flex flex-col items-center h-[90vh]">
         <div className="flex justify-between items-center px-6 py-3">
-        <h2 className="flex items-center text-center">Task Library</h2>
-        <button>
-        <img className="h-6" src="https://res.cloudinary.com/diw7vmgum/image/upload/v1685131977/plus_fftfrp.png" />
-        </button>
+          <h2 className="flex items-center text-center">Task Library</h2>
+          <button>
+            <img
+              className="h-6"
+              src="https://res.cloudinary.com/diw7vmgum/image/upload/v1685131977/plus_fftfrp.png"
+            />
+          </button>
         </div>
         {/* {categories.map((category, idx) => {
           return (
@@ -29,27 +36,30 @@ export default function TaskLibrary({categories}) {
           );
         })} */}
 
-          <SortableContext 
-            items={categories.map((category) => category.name)}
-            strategy={verticalListSortingStrategy}
-          >
-              <div>
-                  {categories.map((category, idx) => {
-                    return (
-                      <div
-                      key={idx}
-                      >
-                      <CategoryItemDnD category={category} />
-                        <div
-                          style={{ backgroundColor: category.color }}
-                          ></div>
-                      </div>
-                    );
-                  })}
-              </div>
-          </SortableContext>
+        <SortableContext
+          items={categories.map((category) => category.name)}
+          strategy={verticalListSortingStrategy}
+        >
+          <div>
+            {categories.map((category, idx) => {
+              return (
+                <div key={idx}>
+                  <CategoryItemDnD category={category} />
+                  <div style={{ backgroundColor: category.color }}></div>
+                </div>
+              );
+            })}
+          </div>
+        </SortableContext>
       </div>
-     
+      <Popup
+        trigger={
+          <button className="modal border border-indigo-500"> Trigger</button>
+        }
+        position="center center"
+      >
+        <NewTaskPopup />
+      </Popup>
     </>
   );
 }
